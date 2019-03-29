7af5e9ebd164196d92e3e66057e7cff58f878040
Bernie Xu <bernie.xu@oocl.com>
2019-03-29 15:11:36 +0800

    change api

--- src/main/java/com/example/vesselstatus/controller/StatusController.java
+++ src/main/java/com/example/vesselstatus/controller/StatusController.java
@@ -3,13 +3,17 @@ package com.example.vesselstatus.controller;
 import com.example.vesselstatus.base.BaseResponse;
 import com.example.vesselstatus.base.Constant;
 
+import com.example.vesselstatus.service.VesselStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/getStatus")
+@RequestMapping("/status")
 @CrossOrigin(maxAge = 3600)
 public class StatusController {
 
+    @Autowired
+    private VesselStatusService vesselStatusService;
 
     @ResponseBody
     @GetMapping("/{vesselGid}")
@@ -19,9 +23,10 @@ public class StatusController {
 //        try { Thread.sleep ( 20000 ) ;
 //        } catch (InterruptedException ie){}
 //        System.out.println("weak up");
-        BaseResponse baseResponse = new BaseResponse();
-        baseResponse.setMsg(Constant.SUCCESS);
-        baseResponse.setCode(Constant.SUCCESS);
-        return baseResponse;
+//        BaseResponse baseResponse = new BaseResponse();
+        return vesselStatusService.findVesselStatusByVesselGid(vesselGid);
+//        baseResponse.setMsg(Constant.SUCCESS);
+//        baseResponse.setCode(Constant.SUCCESS);
+//        return baseResponse;
     }
 }
